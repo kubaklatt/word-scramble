03c138b9701fbfda8eb032b7d35a06bb441da770
Jakub Klatt <jakubklatt@Air-Jakub.lan>
2022-09-15 20:58:21 +0200

    created function for time and randomly generated word with hint

---
+++ src/js/main.js
@@ -0,0 +1,122 @@
+let words = [
+  {
+    word: "addition",
+    hint: "The process of adding numbers",
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together",
+  },
+  {
+    word: "number",
+    hint: "Math symbol used for counting",
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading",
+  },
+  {
+    word: "canvas",
+    hint: "Piece of fabric for oil painting",
+  },
+  {
+    word: "garden",
+    hint: "Space for planting flower and plant",
+  },
+  {
+    word: "position",
+    hint: "Location of someone or something",
+  },
+  {
+    word: "feather",
+    hint: "Hair like outer covering of bird",
+  },
+  {
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation",
+  },
+  {
+    word: "tongue",
+    hint: "The muscular organ of mouth",
+  },
+  {
+    word: "expansion",
+    hint: "The process of increase or grow",
+  },
+  {
+    word: "country",
+    hint: "A politically identified region",
+  },
+  {
+    word: "group",
+    hint: "A number of objects or persons",
+  },
+  {
+    word: "taste",
+    hint: "Ability of tongue to detect flavour",
+  },
+  {
+    word: "store",
+    hint: "Large shop where goods are traded",
+  },
+  {
+    word: "field",
+    hint: "Area of land for farming activities",
+  },
+  {
+    word: "friend",
+    hint: "Person other than a family member",
+  },
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin",
+  },
+  {
+    word: "expert",
+    hint: "Person with extensive knowledge",
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something",
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute",
+  },
+  {
+    word: "library",
+    hint: "Place containing collection of books",
+  },
+];
+
+const wordToGuess = document.querySelector(".word");
+const hintToWord = document.querySelector(".hint-text");
+
+const initGame = () => {
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  let wordArray = randomObj.word;
+  wordToGuess.innerHTML = randomObj.word;
+  hintToWord.innerHTML = randomObj.hint;
+};
+
+initGame();
+
+const timeAmount = document.querySelector(".time-amount");
+
+let seconds = 60;
+
+let time = setInterval(timeLeft, 100);
+
+function timeLeft() {
+  timeAmount.innerHTML = seconds + " seconds";
+  seconds--;
+  if (seconds === 0) {
+    seconds = 0;
+    timeAmount.innerHTML = "You lost, time gone!";
+    clearInterval(time);
+  }
+}
